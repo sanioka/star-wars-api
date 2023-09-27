a5f63c12a6bee68604047b07e6e6b941f5a730cf
Alex Ivanov <isanioka@gmail.com>
2023-09-27 14:52:17 +0300

    Added mobile pagination

--- src/components/CharacterList/CharacterListPagination.tsx
+++ src/components/CharacterList/CharacterListPagination.tsx
@@ -1,9 +1,33 @@
 import { FC, useMemo } from "react";
 import { range } from "../../helpers/range";
-import { Button, Flex } from "@chakra-ui/react";
+import { Button, Flex, Text } from "@chakra-ui/react";
+import { useDevice } from "../../hooks/use-device";
 
 const CharacterListPagination: FC<Props> = ({maxPageCount, currentPage, updateCurrentPage}) => {
+  const {isMobile} = useDevice()
   const paginationList = useMemo(() => range(1, maxPageCount), [maxPageCount])
+
+  if (isMobile) {
+    return (
+      <Flex justifyContent="center" mb={1} mt={4} alignItems='center'>
+        <Button
+          isDisabled={currentPage === 1}
+          onClick={() => updateCurrentPage(currentPage - 1)}
+          m={1}>
+          Prev
+        </Button>
+
+        <Text ml={5} mr={5}>Page {currentPage} of {maxPageCount}</Text>
+
+        <Button
+          isDisabled={currentPage === maxPageCount}
+          onClick={() => updateCurrentPage(currentPage + 1)}
+          m={1}>
+          Next
+        </Button>
+      </Flex>
+    )
+  }
 
   return (
     <Flex justifyContent="center" mb={1} mt={4}>
